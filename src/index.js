334ebe697f06f1fce95a066e9fcb92a042e50924
Чернова Александра <estelelendil@mail.ru>
2022-07-22 15:53:56 +0300

    added plagins for babel slint

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import {
   Swordsman, Bowman, Magician, Daemon, Undead, Zombie,
 } from './js/basic';
 
-const pers = new Daemon('Larry');
-console.log(pers);
+const persDaemon = new Daemon('Larry');
+
+const persSwordsman = new Swordsman('Chack');
+const persBowman = new Bowman('Susanna');
+const persUndead = new Undead('Shon');
+const persMagician = new Magician('Din');
+const persZombie = new Zombie('Will');
+
+console.log(persDaemon, persBowman, persMagician, persSwordsman, persUndead, persZombie);
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,70 +1,65 @@
 class Character {
+  health = 100;
+  level = 1;
+  attack;
+  defence;
   constructor(name, type) {
     this.name = name;
     this.type = type;
     if (typeof this.name !== 'string' || this.name.length > 10 || this.name.length < 2) {
       throw new Error('Имя персонажа введено неверно');
     }
-    if (this.type !== 'Bowman' || 'Swordsman' || 'Magician' || 'Daemon' || 'Undead' || 'Zombie') {
+    if (!['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(this.type)) {
       throw new Error('Тип персонажа введен неверно');
     }
   }
-  health = 100;
-  level = 1;
-  attack;
-  defence;
 }
 
 export class Bowman extends Character {
   constructor(name) {
-    type = 'Bowman';
-    super(name, type, health, level);
-    attach = 25;
-    defence = 25;
+    super(name,  'Bowman');
+    this.attack = 25;
+    this.defence = 25;
   }
 }
 
 export class Swordsman extends Character {
   constructor(name) {
-    type = 'Swordsman';
-    super(name, type, health, level);
-    attach = 40;
-    defence = 10;
+   
+    super(name, 'Swordsman');
+    this.attack = 40;
+    this.defence = 10;
   }
 }
 
 export class Magician extends Character {
   constructor(name) {
-    type = 'Magician';
-    super(name, type, health, level);
-    attach = 10;
-    defence = 40;
+    super(name, 'Magician');
+    this.attack = 10;
+    this.defence = 40;
   }
 }
 
 export class Undead extends Character {
   constructor(name) {
-    type = 'Undead';
-    super(name, type, health, level);
-    attach = 25;
-    defence = 25;
+    super(name, 'Undead');
+    this.attack = 25;
+    this.defence = 25;
   }
 }
 
 export class Zombie extends Character {
   constructor(name) {
-    type = 'Zombie';
-    super(name, type, health, level);
-    attach = 40;
-    defence = 10;
+    super(name, 'Zombie');
+    this.attack = 40;
+    this.defence = 10;
   }
 }
 
 export class Daemon extends Character {
   constructor(name) {
-    type = 'Daemon';
-    super(name, type, health, level);
-    attach = 10;
-    defence = 40;
+    super(name, 'Daemon');
+    this.attack = 10;
+    this.defence = 40;
   }
 }
